90885ed734c1eab0eed091e43a28e0feb484e019
bethurban <beth.urban@gmail.com>
2019-09-09 14:37:16 -0400

    Set root route to searches#index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :searches, only: [:index, :create]
+  root 'searches#index'
+  resources :searches, only: [:create]
   resources :books, only: [:index]
 end
